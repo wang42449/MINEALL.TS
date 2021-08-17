dba6bccbddf1e131cfcdd910736e31142a8d590c
wang42449 <357409629@qq.com>
2021-08-17 08:38:49 +0800

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+  index.js 项目 JS 主入口
+  以依赖 layui 的 layer 和 form 模块为例
+**/
+
+
+layui.define(["layer", "form", "code", "element"], function (exports) {
+  var layer = layui.layer,
+    form = layui.form,
+    code = layui.code,
+    element = layui.element;
+
+  layer.msg("Hello，欢迎");
+
+  exports("index", {}); //注意，这里是模块输出的核心，模块名必须和 use 时的模块名一致
+});
